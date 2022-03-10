9aff407dc66cd75df4fc5f632fa518c693776f43
maggie-j-liu <maggie.j.liu@gmail.com>
2022-03-09 21:52:46 -0800

    update to use list item detail

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,6 +1,6 @@
 import { atom } from "jotai";
 export const maxNumAtom = atom(-1);
-interface Status {
+export interface Status {
   [key: number]: boolean;
 }
 export const readStatusAtom = atom<Status>({});
